759d0ba8ae803f74525ce68e85858300962c9f0a
miriamsilva <miriam-silva@live.com>
2021-11-30 14:35:04 -0800

    front end auth

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,21 +7,19 @@ import { withAuth0 } from '@auth0/auth0-react';
 
 
 class App extends React.Component{
-  constructor(props){
-    super(props);
-    this.state={
-      plant:''
-    };
-  }
-  onButtonClick = async(e) => {
-    //make a request to our server
-    let plant = await axios.get('http://localhost:3001/flower')
-    //save that info into the state
-    console.log(plant);
-    this.setState({
-      plant:plant.data
-    })
-  }
+ makeRequest = async () => {
+   const {getIdTokenClaims} = this.props.auth0;
+   let tokenClaims = await getIdTokenClaims();
+   const jwt = tokenClaims.__raw;
+   
+   const config = {
+     headers:{"Authorization" : `Bearer ${jwt}`}
+   };
+
+   const serverResponse = await axios.get('http://localhost:3001/test-login', config)
+
+   console.log(serverResponse);
+ }
   render(){
     console.log(this.props.auth0);
     const {user, isAuthenticated, isLoading} = this.props.auth0;
@@ -34,11 +32,12 @@ class App extends React.Component{
     return(
       <>
       <h1> Hello!</h1>
-      <button onClick={this.onButtonClick}> Get the data</button>
-      <h2>{this.state.plant}</h2>
-      {isAuthenticated ? <LogoutButton/> : <LoginButton/>}
-      {user ? <h3>{user.name}</h3> : ''}
-      </>
+      {isAuthenticated ? 
+      <LogoutButton/> : <LoginButton/>}
+      {user ? <> 
+      <h3>{user.name}</h3> 
+      <button onClick={this.makeRequest}>Make request to server</button> </> : ''}
+      </> 
     )
     }
   }
