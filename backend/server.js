921cb8611b1aa136cb5993f71b8cb8ace8d2a5f5
miriamsilva <miriam-silva@live.com>
2021-11-30 14:35:22 -0800

    back end auth

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,50 @@
-//Express
-const express = require('express');
-const app = express();
+'use strict';
+const express = require ('express');
+const app = express ();
 
-//Dotenve
-require('dotenv').config();
-const PORT = process.env.PORT;
+require ('dotenv').config ();
 
-//Routes
-app.get('/', (request, response)=>{
-    response.send('hello from the server');
+const cors = require ('cors');
+app.use (cors ());
+
+app.use (express.json ());
+
+//identical code below
+// --------------------
+const jwt = require ('jsonwebtoken');
+const jwksClient = require ('jwks-rsa');
+
+// this function comes directly from the jasonwebtoken docs
+const client = jwksClient ({
+  // this url comes from your app on the auth0 dashboard
+  jwksUri: 'https://miriamsilva.us.auth0.com/.well-known/jwks.json',
 });
 
-app.get('/petName', (request, response)=>{
-    let name = request.query.name;
-    response.send(`Your pet's name is ${name}`);
+const PORT = process.env.PORT || 3001;
+
+// this function comes directly from the jasonwebtoken docs
+function getKey (header, callback) {
+  client.getSigningKey (header.kid, function (err, key) {
+    const signingKey = key.publicKey || key.rsaPublicKey;
+    callback (null, signingKey);
+  });
+}
+
+// ------------------------------
+
+
+app.get ('/test-login', (req, res) => {
+  // grab the token that was sent by the frontend
+  const token = req.headers.authorization.split (' ')[1];
+  // make sure the token was valid
+  jwt.verify (token, getKey, {}, function (err, user) {
+    if (err) {
+      res.status (500).send ('invalid token');
+    } else {
+      res.send (user);
+    }
+  });
 });
 
-app.listen(PORT, ()=> {console.log(`listening on port ${PORT}`)});
+
+app.listen (PORT, () => console.log (`listening on ${PORT}`));
