85a8fcd1c2fecd371bd7501ef69f2aa8566f4681
miriamsilva <miriam-silva@live.com>
2021-11-30 12:19:34 -0800

    scaffolding edits & auth0

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import axios from 'axios';
+import LoginButton from './LoginButton';
+import LogoutButton from './LogoutButton';
+import { withAuth0 } from '@auth0/auth0-react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state={
+      plant:''
+    };
+  }
+  onButtonClick = async(e) => {
+    //make a request to our server
+    let plant = await axios.get('http://localhost:3001/flower')
+    //save that info into the state
+    console.log(plant);
+    this.setState({
+      plant:plant.data
+    })
+  }
+  render(){
+    console.log(this.props.auth0);
+    const {user, isAuthenticated, isLoading} = this.props.auth0;
+
+    if(isLoading){
+      return<h2> Loading...</h2>
+    }
+    else{
+    
+    return(
+      <>
+      <h1> Hello!</h1>
+      <button onClick={this.onButtonClick}> Get the data</button>
+      <h2>{this.state.plant}</h2>
+      {isAuthenticated ? <LogoutButton/> : <LoginButton/>}
+      {user ? <h3>{user.name}</h3> : ''}
+      </>
+    )
+    }
+  }
 }
-
-export default App;
+export default withAuth0(App);
---
+++ frontend/src/LoginButton.js
@@ -0,0 +1,11 @@
+//This code came from auth0: https://auth0.com/docs/quickstart/spa/react
+import React from "react";
+import { useAuth0 } from "@auth0/auth0-react";
+
+const LoginButton = () => {
+  const { loginWithRedirect } = useAuth0();
+
+  return <button onClick={() => loginWithRedirect()}>Log In</button>;
+};
+
+export default LoginButton;
